8fafb0ecb8b8df7f03ca98b6b7c22ece515698ff
ccymbor <ccymbor@gmail.com>
2013-04-24 22:05:52 -0700

    final

--- RobotMovements/Program.cs
+++ RobotMovements/Program.cs
@@ -1,17 +1,57 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        using (StreamReader reader = File.OpenText(args[0]))
-        while (!reader.EndOfStream)
+        int colMax = 4;
+        int rowMax = 4;
+        int[,] usedGrid = new int[rowMax,colMax];
+        int totalPaths = 0;
+
+        WalkGrid(0, 0, ref totalPaths, ref usedGrid);
+        Console.WriteLine(totalPaths);
+    }
+
+    public static void WalkGrid(int startRow, int startCol, ref int totalPaths, ref int[,] usedGrid)
+    {
+        var rowIndexLimit = usedGrid.GetLength(0) - 1;
+        var columnIndexLimit = usedGrid.GetLength(1) - 1;
+
+        if (startRow == rowIndexLimit && startCol == columnIndexLimit) 
         {
-            string line = reader.ReadLine();
-            if (null == line)
-                continue;
-            
+            totalPaths++;
+            return;
         }
+
+        usedGrid[startRow, startCol] = 1;
+
+        //right
+        if (startCol + 1 <= columnIndexLimit && usedGrid[startRow, startCol + 1] == 0)
+        {
+            WalkGrid(startRow, startCol + 1, ref totalPaths, ref usedGrid);
+        }
+
+        //down
+        if (startRow + 1 <= rowIndexLimit && usedGrid[startRow + 1, startCol] == 0)
+        {
+            WalkGrid(startRow + 1, startCol, ref totalPaths, ref usedGrid);
+        }
+        
+        //up
+        if (startRow - 1 >= 0 && usedGrid[startRow - 1, startCol] == 0)
+        {
+            WalkGrid(startRow - 1, startCol, ref totalPaths, ref usedGrid);
+        }
+        
+        //left
+        if (startCol - 1 >= 0 && usedGrid[startRow, startCol - 1] == 0)
+        {
+            WalkGrid(startRow, startCol - 1, ref totalPaths, ref usedGrid);
+        }
+        
+        usedGrid[startRow, startCol] = 0;
     }
 }
