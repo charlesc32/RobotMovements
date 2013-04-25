a8eea460912bebccbce89ab2e13b19c7ec0f0fe7
ccymbor <ccymbor@gmail.com>
2013-04-24 20:11:14 -0700

    init

---
+++ RobotMovements/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+            // do something with line
+        }
+    }
+}
